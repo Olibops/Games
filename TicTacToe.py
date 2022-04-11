e28e4192b43577f957dfbe252c876c09763a9fc1
Olibops <103180548+Olibops@users.noreply.github.com>
2022-04-11 15:03:08 +0100

    Fixed bugs

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,32 +1,36 @@
 def win_check(game):
 
-    if game[0][0] == game[0][1] == game[0][2] and game[0][0] != 0:
+    if game[0][0] == game[0][1] == game[0][2] and game[0][0] != "0":
         return game[0][0]
 
-    if game[1][0] == game[1][1] == game[1][2] and game[1][0] != 0:
+    if game[1][0] == game[1][1] == game[1][2] and game[1][0] != "0":
         return game[1][0]
     
-    if game[2][0] == game[2][1] == game[2][2] and game[2][0] != 0:
+    if game[2][0] == game[2][1] == game[2][2] and game[2][0] != "0":
         return game[2][0]
 
-    if game[0][0] == game[1][0] == game[2][0] and game[0][0] != 0:
+    if game[0][0] == game[1][0] == game[2][0] and game[0][0] != "0":
         return game[0][0]
 
-    if game[0][1] == game[1][1] == game[2][1] and game[0][1] != 0:
+    if game[0][1] == game[1][1] == game[2][1] and game[0][1] != "0":
         return game[0][1]
 
-    if game[0][2] == game[1][2] == game[2][2] and game[0][2] != 0:
+    if game[0][2] == game[1][2] == game[2][2] and game[0][2] != "0":
         return game[0][2]
 
-    if game[0][0] == game[1][1] == game[2][2] and game[0][0] != 0:
+    if game[0][0] == game[1][1] == game[2][2] and game[0][0] != "0":
         return game[0][0]
     
-    if game[2][0] == game[1][1] == game[0][2] and game[1][1] != 0:
+    if game[2][0] == game[1][1] == game[0][2] and game[1][1] != "0":
         return game[0][0]
 
 class Game:
     def __init__(self, game):
         self.game = game
+
+    def show_board(self):
+        for i in range(3):
+            print(self.game[i])
     
     def add_move(self, y):
 
@@ -53,7 +57,7 @@ class Game:
                     scribble="O"
 
 
-                if game[y//3][y%3] == 0:
+                if game[y//3][y%3] == "0":
                     game[y//3][y%3] = scribble
 
                 else:
@@ -65,11 +69,17 @@ class Game:
                 if win_check(game) =="0":
                     print("0 has won")
 
+        for i in range(3):
+            print(self.game[i])
+        print("")
+
         return Game(game)
+
+        
 
 def new_game():
 
-    return Game(game=[[0,0,0],[0,0,0],[0,0,0]])
+    return Game(game=[["0","0","0"],["0","0","0"],["0","0","0"]])
 
 
 
